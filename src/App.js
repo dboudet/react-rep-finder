01a235a388b70a19c1507ae9b72921f20e09f79a
Dan Boudet <dboudet04@gmail.com>
2021-07-26 17:10:15 -0400

    end of day commit

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Footer from './components/footer'
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
       
       <RepresentativeSearch />
 
       <Footer />
-    </div>
+    </>
   );
 }
 
--- src/components/body.js
+++
@@ -1,9 +0,0 @@
-function Main() {
-    return(
-        <main>
-            This is the body
-        </main>
-    )
-}
-
-export default Main
